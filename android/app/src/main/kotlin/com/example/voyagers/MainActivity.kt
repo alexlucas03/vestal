96a1d340a760dda92647c99683bf1d3c3a9e8c80
alexlucas03 <alexlucas03@gmail.com>
2024-12-24 21:51:34 -0800

    init

---
+++ android/app/src/main/kotlin/com/example/voyagers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voyagers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,261 @@
+import 'package:flutter/material.dart';
+import 'package:postgres/postgres.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+Future<void> addMood(String mood) async {
+  try {
+    final conn = await Connection.open(Endpoint(
+      host: 'ep-yellow-truth-a5ebo559.us-east-2.aws.neon.tech',
+      database: 'voyagersdb',
+      username: 'alexlucas03',
+      password: 'Sk3l3ton!sk3l3ton',
+    ));
+    await conn.execute(
+      'INSERT INTO moods (mood) VALUES (@mood)',
+      parameters: {
+        '@mood': mood,
+      },
+    );
+  } catch (e) {
+    print('Error: ${e.toString()}');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF7D9BFF), // Lighter blue for AppBar
+          titleTextStyle: TextStyle(
+            color: Colors.white, // White text in AppBar
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.white), // White text globally
+          bodyText2: TextStyle(color: Colors.white), // White text globally
+        ),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Color(0xFF3A4C7A), // Darkest blue for buttons
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: const Color(0xFF3A4C7A), // Darkest blue for Elevated buttons
+            onPrimary: Colors.white, // White text on buttons
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Voyagers'),
+    );
+  }
+}
+
+Widget _buildSection(BuildContext context, String title, Widget page) {
+  return GestureDetector(
+    onTap: () {
+      // Navigate to the respective page
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => page),
+      );
+    },
+    child: Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: const Color(0xFF0A205A), // Darkest blue for buttons
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Centers the Row content horizontally
+        crossAxisAlignment: CrossAxisAlignment.center, // Centers the Row content vertically
+        children: [
+          Expanded( // Makes the Text take available space and center it
+            child: Center( // Centers the text horizontally and vertically inside the Expanded
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white, // White text color
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            title,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              color: Colors.white, // White text in AppBar
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              child: _buildSection(context, 'Mood Stats', const MoodStats()),
+            ),
+            Expanded(
+              child: _buildSection(context, 'Section 2', const SectionTwoPage()),
+            ),
+            Expanded(
+              child: _buildSection(context, 'Section 3', const SectionThreePage()),
+            ),
+            Expanded(
+              child: _buildSection(context, 'Section 4', const SectionFourPage()),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MoodStats extends StatefulWidget {
+  const MoodStats({super.key});
+
+  @override
+  _MoodStatsState createState() => _MoodStatsState();
+}
+
+class _MoodStatsState extends State<MoodStats> {
+  final TextEditingController _moodController = TextEditingController();
+  String _statusMessage = '';
+
+  void _submitMood() async {
+    String mood = _moodController.text.trim();
+    if (mood.isEmpty) {
+      setState(() {
+        _statusMessage = 'Please enter a mood.';
+      });
+      return;
+    }
+
+    setState(() {
+      _statusMessage = 'Submitting mood...';
+    });
+
+    await addMood(mood);
+
+    setState(() {
+      _statusMessage = 'Mood submitted successfully!';
+    });
+
+    _moodController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mood Stats'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _moodController,
+              decoration: const InputDecoration(
+                labelText: 'Enter your mood',
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _submitMood,
+              child: const Text('Submit Mood'),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              _statusMessage,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SectionTwoPage extends StatelessWidget {
+  const SectionTwoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Section 2 Page'),
+      ),
+      body: const Center(
+        child: Text('You are on Section 2 Page!', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class SectionThreePage extends StatelessWidget {
+  const SectionThreePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Section 3 Page'),
+      ),
+      body: const Center(
+        child: Text('You are on Section 3 Page!', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class SectionFourPage extends StatelessWidget {
+  const SectionFourPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Section 4 Page'),
+      ),
+      body: const Center(
+        child: Text('You are on Section 4 Page!', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
